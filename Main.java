06b7b5b7f2f43047456000abe44558e5a23b6fcc
SergeyShemetun <65458064+SergeyShemetun@users.noreply.github.com>
2021-09-28 05:23:35 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,51 +5,36 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello "+ "user.name");
+        String direction;
+        System.out.println("Hello " + "user.name");
         System.out.println("Welcome to the game");
-        
+
         Help Commands = new Help();
         Commands.printCommands();
-        
+
         System.out.println("Please tell me what to do");
-        Scanner input= new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        while (true) {
-            String Command = input.nextLine();
-            if (Command.equals(Commands[0]))
-            {
-                System.out.println("You went forward");
-            }
-            else if (Command.equals(Commands[1]))
-            {
-                System.out.println("You went back");
-            }
-            else if (Command.equals(Commands[2]))
-            {
-                System.out.println("You went left");
-            }
+        // Setting up user shit
+        User user1 = new User();
+        user1.setName("User 1");
+        Location Location=new Location();
+        Location.setCurrentLocation("Foyer");
 
-            else if (Command.equals(Commands[3]))
-            {
-                System.out.println("You went right");
-            }
-            else if (Command.equals(Commands[4]))
-            {
-                System.out.println("Bye!");
-                break;
+        // Figuring out wtf is  going on
+        Commands.printCommands();
 
-            }
-            else if (Command.equals(Commands[5]))
-            {
-                System.out.println("heres ur help");
-               
-            }
-            else
-                {
-                    System.out.println("I don't know this command");
-                }
+        for(int i=0;i<5;i++) {
+
+            //supposed to start interacting here
+            System.out.println("Which way do you want to go?");
+            direction=input.nextLine();
+            Location.move(direction);
+
+
+
+
 
         }
-
     }
 }
