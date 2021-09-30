3265c858875034c1dbd3f50938b5ce3684c32c25
Duy (Jay) Vu <73553298+Duy-Vux@users.noreply.github.com>
2021-09-30 14:18:39 -0400

    Update Location.java

--- Location.java
+++ Location.java
@@ -20,45 +20,45 @@ public class Location {
 
         if(currentLocation.equals("Foyer"))
             handleFoyerMove(direction);
-        if(currentLocation.equals("Kitchen"))
+        else if(currentLocation.equals("Kitchen"))
             handleKitchenMove(direction);
-        if(currentLocation.equals("Attic"))
+        else if(currentLocation.equals("Attic"))
             handleAtticMove(direction);
-        if(currentLocation.equals("Basement"))
+        else if(currentLocation.equals("Basement"))
             handleBasementMove(direction);
-        System.out.println(" You're updated location is " + currentLocation);
     }
 
     public void handleFoyerMove(String direction) {
-        if(direction.equals(EAST))
+        if(direction.equals(EAST)){
             currentLocation = "Kitchen";
-        else
-            System.out.println(" cant get there from here...  ");
+        }
+        else {
+            System.out.println("Cant get there from here.");
+        }
     }
 
     public void handleKitchenMove(String direction) {
         if(direction.equals(NORTH))
             currentLocation = "Attic";
-        if(direction.equals(SOUTH))
+        else if(direction.equals(SOUTH))
             currentLocation = "Basement";
-        if(direction.equals(WEST))
+        else if(direction.equals(WEST))
             currentLocation = "Foyer";
         else
-            System.out.println(" cant get there from here...  ");
+            System.out.println("Cant get there from here.");
     }
 
     public void handleAtticMove(String direction) {
         if(direction.equals(SOUTH))
             currentLocation = "Kitchen";
         else
-            System.out.println(" cant get there from here...  ");
+            System.out.println("Cant get there from here.");
     }
 
     public void handleBasementMove(String direction) {
         if(direction.equals(NORTH))
             currentLocation = "Kitchen";
         else
-            System.out.println(" cant get there from here...  ");
+            System.out.println("Cant get there from here.");
     }
-
 }
